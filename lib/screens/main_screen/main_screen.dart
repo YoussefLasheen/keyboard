6d771dd494b493ca3681933a8990d9f0253cd8b8
Youssef Lasheen <youssef@lasheen.dev>
2023-02-03 14:19:39 +0200

    Use gtk_window

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
+import 'package:gtk_window/gtk_window.dart';
 import 'package:keyboard/constants.dart';
 import 'package:piano/piano.dart';
 
@@ -23,8 +24,33 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: InteractivePiano(
+    return Scaffold(
+      appBar: GTKHeaderBar(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+              },
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.grey,
+      body: InteractivePiano(
         naturalColor: Colors.white,
         accidentalColor: Colors.black,
         keyWidth: 60,
